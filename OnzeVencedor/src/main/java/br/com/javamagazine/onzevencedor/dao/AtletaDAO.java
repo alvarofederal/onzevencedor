1d799c912f4d97717d9ca15ef61db21289af7f5c
cicero <cicerolmonteiro@gmail.com>
2013-02-28 20:38:40 -0300

    Finalizacao do cadastro de atleta

--- OnzeVencedor/src/main/java/br/com/javamagazine/onzevencedor/dao/AtletaDAO.java
+++ OnzeVencedor/src/main/java/br/com/javamagazine/onzevencedor/dao/AtletaDAO.java
@@ -1,5 +1,74 @@
 package br.com.javamagazine.onzevencedor.dao;
 
-public class AtletaDAO {
+import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.javamagazine.onzevencedor.entity.Atleta;
+
+@Repository("atletaDao")
+public class AtletaDAO {
+	
+	@PersistenceContext
+	protected EntityManager entityManager;
+	
+	@Transactional(readOnly = true)
+	public List<Atleta> findAll() {
+		
+		String jpql = "SELECT a from Atleta a order by a.nome";
+		Query query = this.entityManager.createQuery(jpql);
+		List<Atleta> atletas = query.getResultList();
+		
+		return atletas;
+		
+	}
+	
+	@Transactional(readOnly = true)
+	public List<Atleta> findByClube(Integer idfClube) {
+		
+		String jpql = "SELECT a from Atleta a" +
+				       " JOIN a.clube c" +
+				       " WHERE c.idf = :idfClube" +
+				       " ORDER BY a.nome";
+		Query query = this.entityManager.createQuery(jpql);
+		query.setParameter("idfClube", idfClube);
+		List<Atleta> atletas = query.getResultList();
+		
+		return atletas;
+				
+	}
+	
+	@Transactional
+	public Atleta create(Atleta atleta) {
+		
+		this.entityManager.persist(atleta);
+		
+		return atleta;
+				
+	}
+	
+	@Transactional
+	public void update(Atleta atleta) {
+		
+		this.entityManager.merge(atleta);
+		
+	}
+	
+	@Transactional
+	public void delete(Atleta atleta) {
+			
+		atleta = this.entityManager.find(Atleta.class, atleta.getIdf());
+		this.entityManager.remove(atleta);
+				
+	}
+	
+	public void setEntityManager(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
+	
 }
--- OnzeVencedor/src/main/java/br/com/javamagazine/onzevencedor/service/AtletaService.java
+++ OnzeVencedor/src/main/java/br/com/javamagazine/onzevencedor/service/AtletaService.java
@@ -1,5 +1,51 @@
 package br.com.javamagazine.onzevencedor.service;
 
-public class AtletaService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.javamagazine.onzevencedor.dao.AtletaDAO;
+import br.com.javamagazine.onzevencedor.entity.Atleta;
+
+@Service("atletaService")
+public class AtletaService {
+	
+	@Autowired
+	private AtletaDAO dao;
+	
+	public List<Atleta> getAllAtletas() {
+		
+		return dao.findAll();
+				
+	}
+	
+	public List<Atleta> getAtletasByClube(Integer idfClube) {
+		
+		return dao.findByClube(idfClube);
+				
+	}
+	
+	public Atleta createAtleta(Atleta atleta) {
+		
+		return dao.create(atleta);
+		
+	}
+	
+	public void updateAtleta(Atleta atleta) {
+		
+		dao.update(atleta);
+		
+	}
+	
+	public void deleteAtleta(Atleta atleta) {
+		
+		dao.delete(atleta);
+			
+	}
+	
+	public void setDao(AtletaDAO dao) {
+		this.dao = dao;
+	}
+	
 }
